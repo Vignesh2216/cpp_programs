11e6a6fe46b368f7947d08422377fc70f41df519
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-21 21:31:54 +0530

    datatypes

---
+++ datatype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    float b=1.2;
+    char c='x';
+    string d;
+    cout<<"enter string:";
+    getline(cin,d);
+    cout<<"string:"<<d;
+}
